cb8afc1cb425d98aecaacd515128b18f384621cf
Staffan Gustafsson <info@powercode-consulting.se>
2015-11-22 15:50:58 +0100

    Fixing license file

--- scripts/Publish-ToGallery.ps1
+++ scripts/Publish-ToGallery.ps1
@@ -1,7 +1,7 @@
 $p = @{
     Name = "PSParallel"
     NuGetApiKey = $NuGetApiKey
-    LicenseUri = "https://github.com/powercode/PSParallel/blob/master/LICENSE.txt"
+    LicenseUri = "https://github.com/powercode/PSParallel/blob/master/LICENSE"
     Tag = "Parallel","Runspace","Invoke","Foreach"
     ReleaseNote = "Initial take on Invoke-Parallel, a cmdlet to run script blocks in parallel"
     ProjectUri = "https://github.com/powercode/PSParallel"
