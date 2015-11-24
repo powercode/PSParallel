afc53138132794ec07d07ad9d54ad0b90f79f3e3
Staffan Gustafsson <info@powercode-consulting.se>
2015-11-24 09:45:26 +0100

    Fixing copying of localized files in install script

--- scripts/Install.ps1
+++ scripts/Install.ps1
@@ -4,7 +4,7 @@ $rootDir = Split-Path (Split-Path $MyInvocation.MyCommand.Path)
 
 if ('' -eq $InstallDirectory)
 {
-    $personalModules = Join-Path -Path ([Environment]::GetFolderPath('MyDocuments')) -ChildPath WindowsPowerShell\Modules
+    $personalModules = Join-Path -Path ([Environment]::GetFolderPath('MyDocuments')) -ChildPath WindowsPowerShell\Modules\
     if (($env:PSModulePath -split ';') -notcontains $personalModules)
     {
         Write-Warning "$personalModules is not in `$env:PSModulePath"
@@ -25,14 +25,28 @@ if (!(Test-Path $InstallDirectory))
 
 
 $moduleFileList = @(
-    'PSParallel.psd1'	
-	'en-US\PSParallel.dll-Help.xml'
-	'en-US\about_PSParallel.Help.txt'
-    
+    'PSParallel.psd1'		    
 )
 $binaryFileList = 'src\PsParallel\bin\Release\PSParallel.dll'
+$localizations = @{
+    'en-us'  = @(
+        'PSParallel.dll-Help.xml'
+	    'about_PSParallel.Help.txt'
+    )
+}
 
-
+foreach($kv in $localizations.GetEnumerator())
+{
+    $lang = $kv.Name
+    if(-not (Test-Path $InstallDirectory\$lang))
+    {
+        $null = MkDir $InstallDirectory\$lang
+    }
+    foreach($v in $kv.Value){
+        $locPath = Join-Path $lang $v
+        Copy-Item $rootDir\module\$locPath -Destination $InstallDirectory\$locPath
+    }
+}
 
 $binaryFileList | foreach { Copy-Item "$rootDir\$_" -Destination $InstallDirectory }
 $moduleFileList  | foreach {Copy-Item "$rootdir\module\$_" -Destination $InstallDirectory\$_ }
@@ -41,6 +55,3 @@ Get-ChildItem -Recurse -Path $InstallDirectory
 
 $cert = Get-Item Cert:\CurrentUser\My\98D6087848D1213F20149ADFE698473429A9B15D
 Get-ChildItem -File $InstallDirectory | Set-AuthenticodeSignature -Certificate $cert
-
-    
-    
